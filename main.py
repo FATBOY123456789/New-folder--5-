a10b7798cb398b071f7f8445692e0f76b81b7aaf
FATBOY123456789 <ytzjimbuisness@gmail.com>
2024-10-23 19:12:20 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+class BEEAMDABALU:
+    def costandlooks(self):
+        print('BMW is a quite cheap car and is very good in design')
+
+class Ferara:
+    def costandlooks(self):
+        print('Ferrari is an expensive car and is very good in design too')
+B1=BEEAMDABALU()
+F1=Ferara()
+for c in (B1,F1):
+    c.costandlooks()
